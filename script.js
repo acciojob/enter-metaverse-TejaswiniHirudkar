7d32d27e30e293df96606cc293de1e07a9938f12
Tejaswini Hirudkar <121488475+TejaswiniHirudkar@users.noreply.github.com>
2023-06-22 20:38:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 //your JS code here. If required.
+function updateText() {
+	let element=document.getElementById('status');
+	element.innerHTML=`<h1>Entered Metaverse</h1>`;
+}
+// document.getElementById('enterBtn').addEventListner('click', updateText());
